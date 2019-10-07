caedc7c37e3296899f0ad5dfe39e37f4b8a07978
Keenanks <kks259@nau.edu>
2019-10-06 20:58:40 -0700

    Included title screen, credits, and instructions

--- puzzle_game.js
+++ puzzle_game.js
@@ -12,20 +12,72 @@ var cancelAnimationFrame = window.cancelAnimationFrame ||
 							window.msCancelAnimationFrame;
 							
 var backgroundImg = PIXI.Texture.fromImage("sprites/backgroundExit.png");
+var backgroundTitleImg = PIXI.Texture.fromImage("sprites/background.png");
 var topDoorTexture = PIXI.Texture.fromImage("sprites/doorTop.png");
 var botDoorTexture = PIXI.Texture.fromImage("sprites/doorBottom.png");
+
+var playTexture = PIXI.Texture.fromImage("sprites/playButton.png");
+var creditsTexture = PIXI.Texture.fromImage("sprites/credits.png");
+var instTexture = PIXI.Texture.fromImage("sprites/instr.png");
+var backTextrue = PIXI.Texture.fromImage("sprites/backBtn.png");
 
 
 var renderer = PIXI.autoDetectRenderer({width:1080, height:400, backgroundColor: 0x330033});
 
 gameport.appendChild(renderer.view);
 
+
+
+
+
+
+
+
+/*create all the screens*/
 var stage = new PIXI.Container();
+var game = new PIXI.Container();
+var titleScreen = new PIXI.Container();
+var creditsScreen = new PIXI.Container();
+var instructionScreen = new PIXI.Container();
+
+/*initially have all screens except title screen not visible */
+game.visible = false;
+creditsScreen.visible = false;
+instructionScreen.visible = false;
+titleScreen.visible = true;
+
+/*add screens to parent container */
+stage.addChild(game);
+stage.addChild(creditsScreen);
+stage.addChild(instructionScreen);
+stage.addChild(titleScreen);
+
 var background = new PIXI.Sprite(backgroundImg);
+var titleBack = new PIXI.Sprite(backgroundTitleImg);
 var topDoor1 = new PIXI.Sprite(topDoorTexture);
 var topDoor2 = new PIXI.Sprite(topDoorTexture);
 var botDoor1 = new PIXI.Sprite(botDoorTexture);
 var botDoor2 = new PIXI.Sprite(botDoorTexture);
+
+
+var playBtn = new PIXI.Sprite(playTexture);
+		playBtn.interactive = true;
+		playBtn.buttonMode = true;
+		playBtn.on("mousedown", playGame);
+var creditsBtn = new PIXI.Sprite(creditsTexture);
+		creditsBtn.interactive = true;
+		creditsBtn.buttonMode = true;
+		creditsBtn.on("mousedown", displayCredits);
+var instrBtn = new PIXI.Sprite(instTexture);
+		instrBtn.interactive = true;
+		instrBtn.buttonMode = true;
+		instrBtn.on("mousedown", displayInstructions);
+var backBtn = new PIXI.Sprite(backTextrue);
+		backBtn.interactive = true;
+		backBtn.buttonMode = true;
+		backBtn.on("mousedown", displayTitle);
+		
+		
 
 topDoor1.x = 100;
 topDoor1.y = 0;
@@ -39,14 +91,35 @@ botDoor1.y = 370;
 botDoor2.x = 500;
 botDoor2.y = 370;
 
+playBtn.x = 500;
+playBtn.y = 100;
 
-stage.addChild(background);
-stage.addChild(topDoor1);
-stage.addChild(topDoor2);
-stage.addChild(botDoor1);
-stage.addChild(botDoor2);
+creditsBtn.x = 500;
+creditsBtn.y = 200;
+
+instrBtn.x = 500;
+instrBtn.y = 300;
+
+backBtn.x = 800;
+backBtn.y = 300;
 
 
+
+
+game.addChild(background);
+game.addChild(topDoor1);
+game.addChild(topDoor2);
+game.addChild(botDoor1);
+game.addChild(botDoor2);
+
+text = new PIXI.Text('Made by Keenan Swanson',{fontFamily : 'Arial', fontSize: 24, fill : 0xff1010, align : 'center'});
+
+creditsScreen.addChild(text);
+
+titleScreen.addChild(titleBack);
+titleScreen.addChild(playBtn);
+titleScreen.addChild(creditsBtn);
+titleScreen.addChild(instrBtn);
 
 
 /*StandingStillImages*/
@@ -82,6 +155,41 @@ var yPos = 200;
 var framesRemoved = false;
 var framesAdded = false;
 
+
+function playGame()
+{
+	titleScreen.visible = false;
+	creditsScreen.visible = false;
+	game.visible = true;
+}
+
+function displayCredits()
+{
+	titleScreen.visible = false;
+	creditsScreen.addChild(backBtn);
+	creditsScreen.visible = true;
+	game.visible = false;
+}
+
+function displayInstructions()
+{
+	var text = new PIXI.Text('WASD to move around',{fontFamily : 'Arial', fontSize: 24, fill : 0xff1010, align : 'center'});
+	titleScreen.visible = false;
+	creditsScreen.visible = false;
+	game.visible = false;
+	instructionScreen.addChild(backBtn);
+	instructionScreen.addChild(text);
+	instructionScreen.visible = true;
+}
+
+function displayTitle()
+{
+	titleScreen.visible = true;
+	creditsScreen.visible = false;
+	game.visible = false;
+	instructionScreen.visible = false;
+}
+
 function ready(){
 	
 	player = new PIXI.AnimatedSprite(frames);
@@ -92,7 +200,7 @@ function ready(){
 	player.position.y = yPos;
 	player.play();
 	
-	stage.addChild(player);
+	game.addChild(player);
 	
 	
 	
@@ -111,6 +219,13 @@ function ready(){
 		{
 			key = 87;
 			walkUp();
+		}
+		
+		if(e.keyCode == 27)
+		{
+			key = 27;
+			game.visible = false;
+			titleScreen.visible = true;
 		}
 		
 		if(e.keyCode == 83)
@@ -144,8 +259,8 @@ function ready(){
 			framesAdded = true;
 		}
 		
-		yPos -= 6;
-		createjs.Tween.get(player.position).to({y: yPos});
+		yPos -= 12;
+		createjs.Tween.get(player.position).to({y: yPos}, 100 , createjs.Ease.linear);
 		
 	}
 	
@@ -161,8 +276,8 @@ function ready(){
 			framesAdded = true;
 		}
 		
-		player.y += 6;
-		
+		yPos += 12;
+		createjs.Tween.get(player.position).to({y: yPos}, 100 , createjs.Ease.linear);
 	}
 	
 	function walkLeft()
@@ -177,8 +292,8 @@ function ready(){
 			framesAdded = true;
 		}
 		
-		player.x -= 6;
-		
+		xPos -= 12;
+		createjs.Tween.get(player.position).to({x: xPos}, 100 , createjs.Ease.linear);
 	}
 	
 	function walkRight()
@@ -193,7 +308,8 @@ function ready(){
 			framesAdded = true;
 		}
 		
-		player.x += 6;
+		xPos += 12;
+		createjs.Tween.get(player.position).to({x: xPos}, 100 , createjs.Ease.linear);
 		
 	}
 	
